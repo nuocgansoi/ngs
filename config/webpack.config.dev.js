2b9ca2fedc235d0e8c51f904e6020ba0f33394ea
nuocgansoi <nuocgansoi@gmail.com>
2018-04-23 16:57:58 +0700

    [+] scss loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -74,7 +74,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/253
     modules: ['node_modules', paths.appNodeModules].concat(
       // It is guaranteed to exist because we tweak it in `env.js`
-      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean),
     ),
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
@@ -84,7 +84,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -115,7 +115,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -144,7 +144,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
@@ -156,34 +156,49 @@ module.exports = {
           // "style" loader turns CSS into JS modules that inject <style> tags.
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
+          //{
+          //  test: /\.css$/,
+          //  use: [
+          //    require.resolve('style-loader'),
+          //    {
+          //      loader: require.resolve('css-loader'),
+          //      options: {
+          //        importLoaders: 1,
+          //      },
+          //    },
+          //    {
+          //      loader: require.resolve('postcss-loader'),
+          //      options: {
+          //        // Necessary for external CSS imports to work
+          //        // https://github.com/facebookincubator/create-react-app/issues/2677
+          //        ident: 'postcss',
+          //        plugins: () => [
+          //          require('postcss-flexbugs-fixes'),
+          //          autoprefixer({
+          //            browsers: [
+          //              '>1%',
+          //              'last 4 versions',
+          //              'Firefox ESR',
+          //              'not ie < 9', // React doesn't support IE8 anyway
+          //            ],
+          //            flexbox: 'no-2009',
+          //          }),
+          //        ],
+          //      },
+          //    },
+          //  ],
+          //},
           {
-            test: /\.css$/,
+            test: /\.scss$/,
             use: [
-              require.resolve('style-loader'),
               {
-                loader: require.resolve('css-loader'),
+                loader: 'style-loader' // creates style nodes from JS strings
+              }, {
+                loader: 'css-loader' // translates CSS into CommonJS
+              }, {
+                loader: 'sass-loader', // compiles Sass to CSS
                 options: {
                   importLoaders: 1,
-                },
-              },
-              {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
-                  ident: 'postcss',
-                  plugins: () => [
-                    require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }),
-                  ],
                 },
               },
             ],
@@ -198,7 +213,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -212,6 +212,23 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.scss$/,
+            use: [
+              {
+                loader: 'style-loader' // creates style nodes from JS strings
+              }, {
+                loader: 'css-loader' // translates CSS into CommonJS
+              }, {
+                loader: 'sass-loader', // compiles Sass to CSS
+                options: {
+                  importLoaders: 1,
+                  minimize: true,
+                  sourceMap: shouldUseSourceMap,
+                },
+              },
+            ],
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
@@ -222,7 +239,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
