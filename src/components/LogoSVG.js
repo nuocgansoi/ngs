59009d2731fbf81d4584a7775c7772cc71795545
nuocgansoi <nuocgansoi@gmail.com>
2019-03-15 19:54:01 +0700

    [+] animate logo

--- src/components/LogoSVG.js
+++ src/components/LogoSVG.js
@@ -15,7 +15,7 @@ export default (props) => (
         }
 
           .cls-2 {
-          fill: url(#linear-gradient);
+          fill: none;
         }
 
           .cls-3 {
@@ -31,14 +31,14 @@ export default (props) => (
       <g id="logo" transform="translate(-104 872)">
         <g id="S" transform="translate(-146 -24)">
           <rect id="Rectangle_88" data-name="Rectangle 88" class="cls-1" width="208" height="208" transform="translate(941 304)"/>
-          <path id="Union_1" data-name="Union 1" class="cls-2" d="M446.991-921.019h0l-9.765-13.946,0,0L417.85-962.646l9.766-13.947L447-948.917,500.269-1025H374v-.269l-31.387-44.826,1.8-1.26,38.931-55.6V-1129H515.379l-11.3,16H393.105l-30.5,43.56L382.518-1041H531L456.761-934.97v0L447-921.021v0l0,0,0,0h0Zm0,0h0Z" transform="translate(608.387 1433)"/>
+          <path stroke="#5ff17efa" stroke-width="10" id="Union_1" data-name="Union 1" class="cls-2" d="M446.991-921.019h0l-9.765-13.946,0,0L417.85-962.646l9.766-13.947L447-948.917,500.269-1025H374v-.269l-31.387-44.826,1.8-1.26,38.931-55.6V-1129H515.379l-11.3,16H393.105l-30.5,43.56L382.518-1041H531L456.761-934.97v0L447-921.021v0l0,0,0,0h0Zm0,0h0Z" transform="translate(608.387 1433)"/>
         </g>
         <g id="G" transform="translate(260 250)">
-          <path id="Union_2" data-name="Union 2" class="cls-2" d="M32-914v-4H-136v4h-16v-4h-4v-16h4v-168h-4v-16h4v-4h16v4H32v-4H48v4h4v16H48v4H32v-4H-136v168H32v-72H-32v23H-48v-23h-4v-16h4v-4h16v4H32v-4H48v4h4v16H48v72h4v16H48v4Z" transform="translate(482.999 1152)"/>
+          <path stroke="#5ff17efa" stroke-width="10"  id="Union_2" data-name="Union 2" class="cls-2" d="M32-914v-4H-136v4h-16v-4h-4v-16h4v-168h-4v-16h4v-4h16v4H32v-4H48v4h4v16H48v4H32v-4H-136v168H32v-72H-32v23H-48v-23h-4v-16h4v-4h16v4H32v-4H48v4h4v16H48v72h4v16H48v4Z" transform="translate(482.999 1152)"/>
         </g>
         <g id="N" transform="translate(296 -107)">
           <rect id="Rectangle_53" data-name="Rectangle 53" class="cls-3" width="208" height="208" transform="translate(67 387)"/>
-          <path id="Union_1-2" data-name="Union 1" class="cls-2" d="M-57.237-561.3H-176.925l-2.467,3.157L-192-568-98.39-687.812-151-755.15l12.608-9.85,2.888,3.7h94.532l2.888-3.7,12.608,9.85-52.61,67.338L-30.805-627.3,1.9-686.3,15.9-678.547l-36,64.949L15.523-568l-12.608,9.85L-28.531-598.4-51.006-557.85Zm-1.279-16,19.284-34.789-49.006-62.725L-164.424-577.3Zm-29.722-123.5,34.765-44.5H-123Z" transform="translate(259 1152.304)"/>
+          <path stroke="#5ff17efa" stroke-width="10"  id="Union_1-2" data-name="Union 1" class="cls-2" d="M-57.237-561.3H-176.925l-2.467,3.157L-192-568-98.39-687.812-151-755.15l12.608-9.85,2.888,3.7h94.532l2.888-3.7,12.608,9.85-52.61,67.338L-30.805-627.3,1.9-686.3,15.9-678.547l-36,64.949L15.523-568l-12.608,9.85L-28.531-598.4-51.006-557.85Zm-1.279-16,19.284-34.789-49.006-62.725L-164.424-577.3Zm-29.722-123.5,34.765-44.5H-123Z" transform="translate(259 1152.304)"/>
         </g>
       </g>
       `,
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,60 @@ import TheEye from "../pages/Park/TheEye.js";
 import routes from '../routes';
 import LogoSVG from './LogoSVG';
 
-export default (props) => (
-  <nav id="navbar" {...props}>
-    <div className="body container">
-      <div className="d-flex justify-content-between">
-        <Link to={routes.home}>
-          <LogoSVG/>
-        </Link>
+export default class extends React.Component {
+  componentDidMount() {
+    const svgs = this.refs.navbar.querySelectorAll('svg');
+    console.log(svgs);
+    for (let i = 0; i < svgs.length; i++) {
+      const svg = svgs[i];
+      const path = svg.querySelector('path');
+      if (!path) continue;
 
-        <div className="d-flex">
-          <TheEye width={40} height={20}/>
-          <TheEye width={40} height={20}/>
+      const pathLength = path.getTotalLength();
+      svg.style.strokeDasharray = pathLength / 5;
+      svg.style.strokeDashoffset = pathLength;
+
+      // Make style tag
+      const style = document.createElement('style');
+      style.type = 'text/css';
+
+      const animateName = `navbarSvgDash${i}`;
+      style.innerHTML = `
+      @keyframes ${animateName} {
+        from {
+          stroke-dashoffset: ${pathLength};
+        }
+        to {
+          stroke-dashoffset: 0;
+        }
+      }
+    `;
+      document.getElementsByTagName('head')[0].appendChild(style);
+
+      svg.style.animation = `${animateName} linear 5s alternate infinite`;
+    }
+  }
+
+  render() {
+    return (
+      <nav
+        ref="navbar"
+        id="navbar"
+        {...this.props}
+      >
+        <div className="body container">
+          <div className="d-flex justify-content-between">
+            <Link to={routes.home}>
+              <LogoSVG/>
+            </Link>
+
+            <div className="d-flex">
+              <TheEye width={40} height={20}/>
+              <TheEye width={40} height={20}/>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </nav>
-);
+      </nav>
+    )
+  }
+}
