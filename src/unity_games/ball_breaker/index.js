3d018ea7a6a8857d536fe255b1e484bd1c3536b4
nuocgansoi <nuocgansoi@gmail.com>
2023-07-12 23:42:18 +0700

    [+] unity game: ball breaker v1.1

--- src/unity_games/ball_breaker/index.js
+++ src/unity_games/ball_breaker/index.js
@@ -3,10 +3,10 @@ import UnityWrapper from "../../components/UnityWrapper";
 
 function BallBreaker() {
   const unityContext = {
-    loaderUrl: "/unity_games/ball_breaker/Build/v1.loader.js",
-    dataUrl: "/unity_games/ball_breaker/Build/v1.data",
-    frameworkUrl: "/unity_games/ball_breaker/Build/v1.framework.js",
-    codeUrl: "/unity_games/ball_breaker/Build/v1.wasm",
+    loaderUrl: "/unity_games/ball_breaker/Build/v1.1.loader.js",
+    dataUrl: "/unity_games/ball_breaker/Build/v1.1.data",
+    frameworkUrl: "/unity_games/ball_breaker/Build/v1.1.framework.js",
+    codeUrl: "/unity_games/ball_breaker/Build/v1.1.wasm",
   };
 
   return (
